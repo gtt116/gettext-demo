a7267aa8bc9eb784379433f746dd8a956ddf6c61
fy <fy0748@gmail.com>
2013-09-25 12:47:41 +0800

    windows supported.
    Test on windows 8.

--- tt.py
+++ tt.py
@@ -1,6 +1,14 @@
 # -*- coding:utf-8 -*-
 
+import os, sys
 import gettext
+
+if sys.platform.startswith('win'):
+    import locale
+    if os.getenv('LANG') is None:
+        lang, enc = locale.getdefaultlocale()
+        os.environ['LANG'] = lang
+
 
 gettext.install('tt', './locale', unicode=1)
 
